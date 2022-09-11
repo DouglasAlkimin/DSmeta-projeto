72c72a22053d3bc699261c038bb86e1cc7383707
DouglasM <douglasmikael999@gmail.com>
2022-09-11 13:12:41 -0300

    Axios, useEffect first request

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,9 +6,7 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
-                <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/douglas_alkiminn">@Douglas_Alkiminn</a>
+                <p> Desenvolvido por <a href="https://www.instagram.com/douglas_alkiminn">@Douglas_Alkiminn</a>
                 </p>
             </div>
         </header>
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -7,15 +8,22 @@ import NotificationButton from '../NotificationButton';
 import './styles.css';
 
 function SalesCard() {
-   
+
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
-    const max = new Date ();
+    const max = new Date();
 
-    const [minDate, setMinDate] = useState (min);
-    const [maxDate, setMaxDate] = useState (max);
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
 
-    
 
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+            .then(response => {
+
+           console.log(response.data);
+
+            });
+    }, []);
 
     return (
         <div className="dsmeta-card">
@@ -24,7 +32,7 @@ function SalesCard() {
                 <div className="dsmeta-form-control-container">
                     <DatePicker
                         selected={minDate}
-                        onChange={(date: Date) => setMinDate (date)}
+                        onChange={(date: Date) => setMinDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
@@ -32,7 +40,7 @@ function SalesCard() {
                 <div className="dsmeta-form-control-container">
                     <DatePicker
                         selected={maxDate}
-                        onChange={(date: Date) => setMaxDate (date)}
+                        onChange={(date: Date) => setMaxDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
